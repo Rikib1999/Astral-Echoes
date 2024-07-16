dd0228a9930b5d868ecba4b26ccc0cc48ae5a291
Belster24 <brossjefrajer@gmail.com>
2024-07-16 18:57:52 +0200

    Crafting
    Started the crafting system
    You can fill the craft items into the system, need to write what it needs
    It also check if the items are in the inventory so you can craft it or not

--- Assets/Scripts/Inventory/Crafting/CraftManager.cs
+++ Assets/Scripts/Inventory/Crafting/CraftManager.cs
@@ -11,6 +11,30 @@ public class CraftManager : MonoBehaviour
     public Transform craftContent;
     public GameObject craft;
 
+    public void checkItems()
+    {
+        // Get the inventory instance
+        InventoryManager inventoryManager = InventoryManager.Instance;
+
+        // Iterate through all crafts
+        foreach (var craftItem in crafts)
+        {
+            // Check if required items are present in the inventory
+            bool hasItemNeeded1 = inventoryManager.items.Exists(item => item.itemName == craftItem.itemNeeded1);
+            bool hasItemNeeded2 = inventoryManager.items.Exists(item => item.itemName == craftItem.itemNeeded2);
+
+            if (hasItemNeeded1 && hasItemNeeded2)
+            {
+                Debug.Log($"You have the items needed to craft {craftItem.craftName}");
+            }
+            else
+            {
+                Debug.Log($"You do not have the items needed to craft {craftItem.craftName}");
+            }
+        }
+
+    }
+
 
    
     public void ListItems()
@@ -26,9 +50,13 @@ public class CraftManager : MonoBehaviour
             GameObject newItem = Instantiate(craft, craftContent);
             var ItemName = newItem.transform.Find("CraftName").GetComponent<TMPro.TextMeshProUGUI>();
             var ItemIcon = newItem.transform.Find("Image").GetComponent<UnityEngine.UI.Image>();
+            var ItemNeeded1 = newItem.transform.Find("ItemNeeded1").GetComponent<TMPro.TextMeshProUGUI>();
+            var ItemNeeded2 = newItem.transform.Find("ItemNeeded2").GetComponent<TMPro.TextMeshProUGUI>();
 
             ItemName.text = item.craftName;
             ItemIcon.sprite = item.icon;
+            ItemNeeded1.text = item.itemNeeded1;
+            ItemNeeded2.text = item.itemNeeded2;
         }
     }
 }
