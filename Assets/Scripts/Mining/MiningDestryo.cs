f78d44d549f11b41c510b2a03b4d8c4302c59323
Belster24 <brossjefrajer@gmail.com>
2024-07-17 21:22:59 +0200

    Mining
    Did some basic mining, you need to put item which you want to be droped, or it can be done faster with the prefabs (did not do it)

---
+++ Assets/Scripts/Mining/MiningDestryo.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiningDestryo : MonoBehaviour
+{
+    public int health = 3;
+
+    public GameObject item;
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        Debug.Log("Health: " + health);
+
+        if (health <= 0)
+        {
+            SpanwItem();
+            Destroy(gameObject);
+            
+        }
+    }
+
+    public void OnMouseDown()
+    {
+        TakeDamage(1);
+    }
+
+    private void SpanwItem()
+    {
+        if (item != null)
+        {
+            Instantiate(item, transform.position, transform.rotation);
+        }
+        else
+        {
+            Debug.LogWarning("Capsule prefab is not assigned.");
+        }
+    }
+    
+
+}
