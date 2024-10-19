a774e6a3cf6d4477320bf138fd84ae216ceec34b
Rikib1999 <74504682+Rikib1999@users.noreply.github.com>
2024-10-19 14:26:10 +0200

    spaceship interior scene

---
+++ Assets/Scripts/SpaceshipInterior/BackgroundScroller.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+namespace Assets.Scripts.SpaceshipInterior
+{
+    public class BackgroundScroller : MonoBehaviour
+    {
+        private RawImage rawImage;
+
+        [SerializeField] private float multiplier;
+
+        void Start()
+        {
+            rawImage = GetComponent<RawImage>();
+        }
+
+        void Update()
+        {
+            rawImage.uvRect = new Rect(rawImage.uvRect.position + new Vector2(rawImage.uvRect.position.x, multiplier) * Time.deltaTime, rawImage.uvRect.size);
+        }
+    }
+}
