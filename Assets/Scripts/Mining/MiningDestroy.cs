ac60417ec33080bb3ba25effa635df29b0cd4dd9
Belster24 <brossjefrajer@gmail.com>
2024-11-30 14:40:24 +0100

    Better drops from resources
    Fixed drops from resources, so it is better.
    Added food, meat and fuel

--- Assets/Scripts/Mining/MiningDestroy.cs
+++ Assets/Scripts/Mining/MiningDestroy.cs
@@ -5,6 +5,9 @@ public class MiningDestryo : MonoBehaviour
 {
     public int health = 3;
     public GameObject item;
+    public GameObject DropItem;
+    public List<Item> lootTable;
+
     public void TakeDamage(int damage)
     {
         health -= damage;
@@ -24,11 +27,53 @@ public class MiningDestryo : MonoBehaviour
     {
         if (item != null)
         {
-            Instantiate(item, transform.position, transform.rotation);
+            //Instantiate(item, transform.position, transform.rotation);
+            InstantiateLoot(transform.position);
         }
         else
         {
             Debug.LogWarning("Capsule prefab is not assigned.");
+        }
+    }
+
+
+    Item GetDroppedItem()
+    {
+        int randomNumber = Random.Range(0, lootTable.Count + 1);
+
+        List<Item> possibleItems = new List<Item>();
+
+        for (int i = 0; i < lootTable.Count; i++)
+        {
+            if (i == randomNumber)
+            {
+                //Instantiate(lootTable[i], transform.position, transform.rotation);
+                possibleItems.Add(lootTable[i]);
+
+            }
+
+        }
+
+        if (possibleItems.Count > 0)
+        {
+            Item droppedItem = possibleItems[Random.Range(0, possibleItems.Count)];
+            return droppedItem;
+        }
+        Debug.Log("No loot dropped");
+        return null;
+    }
+
+    public void InstantiateLoot(Vector2 spawnPos)
+    {
+        Item droppedItem = GetDroppedItem();
+
+        if (droppedItem != null)
+        {
+            GameObject itemGameObject = Instantiate(DropItem, spawnPos, Quaternion.identity);
+
+            itemGameObject.GetComponent<SpriteRenderer>().sprite = droppedItem.icon;
+
+            itemGameObject.GetComponent<ItemPickUp>().item = droppedItem;
         }
     }
 
